fe49873f2f559318d1ce0c3337f46e85c855735c
Tan Kok Boon <kbtan@seekasia.com>
2021-03-06 16:22:08 +0800

    fix redirect chain issue

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # reverse-me
+
+Principle
+Decision Log
+
+Lesson Learnt
+* Redirect loop
+* flush(cb) vs flush: () => {}
+
+connection speed
+
+tech selection: solution - options
+why not jsdom
+why not node-fetch / highland
+
+limitation:
+- only https
+- browser compatability
+- redirection
+- hyperlink not working when redirect 
+- content-encoding: 'gzip'
+ proxiedRes.pipe(zlib.createGunzip()).pipe(reverse()).pipe(res)
+
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const https = require('https');
 const http = require('http');
+const url = require('url');
 const { Transform } = require('stream');
 
 const flipStyles = `
@@ -29,26 +30,48 @@ const reverse = () => {
   })
 };
 
-const getPage = (options) => {
-  return new Promise((resolve, reject) => {
-    https.get(options, (proxiedRes) => {
-      resolve(proxiedRes); 
-    });
-  });
-}
-console.log(fqdn);
-const proxy = async (req, res) => {
-  const options = {
+const getOptions = (fqdn, path) => {
+  return {
     hostname: `${fqdn}`,
     port: 443,
-    path: req.url,
+    path, 
     method: 'GET',
     headers: { 'host': `${fqdn}` }
   };
+}
+
+const getPage = (options, redirectLoop = 0) => {
+  return new Promise((resolve, reject) => {
+    if (redirectLoop > 5) {
+      reject(`Too many redirection(${redirectLoop})`);
+    }
+    https.get(options, (proxiedRes) => {
+      const {statusCode, headers: { location = '' } } = proxiedRes;
+      const hostname = url.parse(location).hostname; 
+      if (statusCode > 299 && statusCode < 400) {
+        const options = getOptions((hostname ? hostname : fqdn), location)
+        resolve({type: 'redirect', options});
+      } else {
+        resolve({type: 'complete', result: proxiedRes });
+      }
+    });
+  }).then((chain) => {
+    const { type, options, result } = chain;
+    if(type === 'complete') {
+      return result; 
+    }
+    if(type === 'redirect') {
+      return getPage(options, redirectLoop + 1);
+    }
+    throw new Error("Something went wrong in redirect chain");
+  });
+}
+
+const proxy = async (req, res) => {
   try {
-    const proxiedRes = await getPage(options);
+    const proxiedRes = await getPage(getOptions(fqdn, req.url));
     res.writeHead(proxiedRes.statusCode, proxiedRes.headers);
-    proxiedRes.pipe(reverse()).pipe(res);
+    proxiedRes.pipe(reverse()).pipe(res)
   } catch(e) {
     console.log(e);
   }
